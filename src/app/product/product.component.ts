50cb5b77ea3896d6b3b05e79da6171eddd39ecac
HAND\Administrator <254564964@qq.com>
2018-08-02 17:24:08 +0800

    08.02

---
+++ src/app/product/product.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent implements OnInit {
+  private products:Array<Product>;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.products = [
+      new Product(1,"第一个商品",1.99,3.5,"商品描述",["电子商品","硬件设备"]),
+      new Product(2,"第二个商品",1.99,2.5,"商品描述",["电子商品","硬件设备"]),
+      new Product(3,"第三个商品",1.99,4.5,"商品描述",["电子商品","硬件设备"]),
+      new Product(4,"第四个商品",1.99,1.5,"商品描述",["电子商品","硬件设备"]),
+      new Product(5,"第五个商品",1.99,3,"商品描述",["电子商品","硬件设备"]),
+      new Product(6,"第六个商品",1.99,5,"商品描述",["电子商品","硬件设备"])
+    ]
+  }
+
+}
+
+export class Product{
+  constructor(
+    public id:number,
+    public title:string,
+    public price:number,
+    public rating:number,
+    public desc:string,
+    public categories:Array<string>
+  ){ }
+}
